a9fababed58ec0992ff65004d69e04a69f394731
Bodnarchuk Valerii <bodnarchukvalole@gmail.com>
2018-12-19 15:24:58 +0200

    Change dropdown, make sticky footer and little fixes

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,17 @@ $("a[href^='#']").on('click', function (event) {
 
 // Header DropDown
 
-$(".header-cart").on('click', function(){
-	$(".header-dropdown").toggleClass("header-dropdown_active");
-    $(".header-cart__icon").toggleClass("header-cart__icon_active");
+$(".header-cart").click(function(){
+   	$(".header-dropdown").addClass("header-dropdown_active");
+   	$(".header-cart__icon").addClass("header-cart__icon_active");
+});
+
+jQuery(function($){
+	$(document).mouseup(function (e){
+		let dropDown = $("#dropdown");
+		if (!dropDown.is(e.target) && dropDown.has(e.target).length === 0) {
+		   	$(".header-dropdown").removeClass("header-dropdown_active");
+   			$(".header-cart__icon").removeClass("header-cart__icon_active");
+		}
+	});
 });
