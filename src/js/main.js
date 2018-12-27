2bf998a95cc15fcf3bfd0fa1844e3f44021d1612
Bodnarchuk Valerii <bodnarchukvalole@gmail.com>
2018-12-27 17:13:54 +0200

    Create adaptive for all devices

--- src/js/main.js
+++ src/js/main.js
@@ -104,21 +104,21 @@ $('.cook-slider__list').slick({
 	slidesToShow: 4,
 	responsive: [
 		{
-			breakpoint: 1024,
+			breakpoint: 1200,
 			settings: {
 				slidesToShow: 3,
 				slidesToScroll: 3
 			}
 		},
 		{
-			breakpoint: 768,
+			breakpoint: 900,
 			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 2
 			}
 		},
 		{
-			breakpoint: 501,
+			breakpoint: 620,
 			settings: {
 				slidesToShow: 1,
 				slidesToScroll: 1
