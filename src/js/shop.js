8eaf72e4c5e3ba0a91cf86099ac49cd031738a86
Bodnarchuk Valerii <bodnarchukvalole@gmail.com>
2019-01-04 16:29:15 +0200

    Create adaptive for all devices, add another animations

--- src/js/shop.js
+++ src/js/shop.js
@@ -3,4 +3,8 @@ function shop() {
 		e.preventDefault();
 		$(this).toggleClass("product__like_active")
 	});
+	$(".product-wrap__circle").on('click', function(e){
+		e.preventDefault();
+		$(this).toggleClass("product-wrap__circle_active")
+	});
 }
